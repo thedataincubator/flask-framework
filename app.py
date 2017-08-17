9d3e8212d9b7e539546e59cc4ffe1760280b3fc3
Robert Schroll <rschroll@gmail.com>
2017-08-17 15:50:13 -0700

    Remove redirect from / to /index
    Just serve at /.

--- app.py
+++ app.py
@@ -3,10 +3,6 @@ from flask import Flask, render_template, request, redirect
 app = Flask(__name__)
 
 @app.route('/')
-def main():
-  return redirect('/index')
-
-@app.route('/index')
 def index():
   return render_template('index.html')
 
