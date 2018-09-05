b612928b06227804c200bc44ea024461494532c9
whitehdt <whitehdt@gmail.com>
2018-09-04 22:42:46 -0400

    add plot function

--- app.py
+++ app.py
@@ -11,8 +11,8 @@ def index():
     if request.method == 'GET':
         return render_template('user_inputs_new.html')
 
-@app.route('/about')
-def about():
+@app.route('/plot',methods=['POST'])
+def plot():
   return render_template('about.html')
 
 if __name__ == '__main__':
