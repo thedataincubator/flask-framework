44e73606e4b1214e63aa2e67b286afabccb4cde9
whitehdt <whitehdt@gmail.com>
2018-09-04 22:34:44 -0400

    Add spacing

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 @app.route('/',methods=['GET','POST'])
 def index():
-  return render_template('user_inputs_new.html')
+    return render_template('user_inputs_new.html')
 
 @app.route('/about')
 def about():
