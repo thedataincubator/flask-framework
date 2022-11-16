859fdf1d16fa14e1be263185d210e8e36dbc25d7
Russell Martin <russell@martinandbrown.com>
2022-11-16 15:34:29 -0500

    Remove unneeded imports

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -11,4 +11,4 @@ def about():
   return render_template('about.html')
 
 if __name__ == '__main__':
-  app.run(port=33507)
+  app.run()
